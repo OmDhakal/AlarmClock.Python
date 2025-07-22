14fbdf9cf5f4c250d802a43f237591200a3b5fb9
OmDhakal <odhakal127@gmail.com>
2025-07-22 17:05:07 +0545

    Add files via upload
    Learned Layout Manegers

--- pyqtPractise.py
+++ pyqtPractise.py
@@ -1,15 +1,48 @@
 #PyQt5 introduction
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QApplication, QMainWindow ,QLabel ,
+                             QWidget , QVBoxLayout, QHBoxLayout, QGridLayout)
+ 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My first GUI")
         self.setGeometry(700, 300, 500, 500)
-        self.setWindowIcon(QIcon("C:/Users/odhak/Downloads/berserk.jpg.webp"))
+        self.initUI()
 
+
+
+    def initUI(self):
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        label1 = QLabel("#1", self)
+        label2 = QLabel("#2", self)
+        label3 = QLabel("#3", self)
+        label4 = QLabel("#4", self)
+        label5 = QLabel("#5", self)
+
+        label1.setStyleSheet("background-color: red;")
+        label2.setStyleSheet("background-color: green;")
+        label3.setStyleSheet("background-color: yellow;")
+        label4.setStyleSheet("background-color: blue;")
+        label5.setStyleSheet("background-color: purple;")
+
+        #vbox = QVBoxLayout() #Verticall Layout
+        #vbox = QHBoxLayout() #Horizontal Layout
+
+        grid = QGridLayout()
+
+        grid.addWidget(label1, 0, 0)
+        grid.addWidget(label2, 0, 1)
+        grid.addWidget(label3, 1, 0)
+        grid.addWidget(label4, 2, 0)
+        grid.addWidget(label5, 3, 0)
+
+        central_widget.setLayout(grid)
+        
+        
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
